e2dfed18495ac37bf3eb4f6149f586a08383fe23
sebastian a mathias <sebastianmathias@ext.cvccorp.com.br>
2020-02-27 12:21:45 -0300

    Adicao da funcao salvar_em_json() e pasta data

--- abducaoComRspec/spec/mapear_intel__spec.rb
+++ abducaoComRspec/spec/mapear_intel__spec.rb
@@ -1,13 +1,20 @@
 describe 'mapear dados' do
+    require 'json'
     def pegar_resultados()
-        @acessar = Acesso.get('/Transparencia/vencimentos2') 
-        puts @acessar
+        @servidores = Acesso.get('/Transparencia/vencimentos2') 
+        puts @servidores
+    end
+
+    def salvar_em_json()
+        json_file = File.new("data/servidores.json", "w")
+        json_file.puts(@servidores)
+        json_file.close
     end
     
     context 'mapear' do
         before { pegar_resultados()}
         it 'com sucesso' do
-            
+            salvar_em_json()
         end
     end
 end
